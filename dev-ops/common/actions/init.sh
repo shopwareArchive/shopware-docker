a19cc4a5cc0af7c3de596135a3b18eaaee7f3cc1
Thomas Eiling <teiling88@gmail.com>
2017-09-08 16:21:38 +0200

    adding possibility to select shopware version during init process

--- dev-ops/common/actions/init.sh
+++ dev-ops/common/actions/init.sh
@@ -2,4 +2,4 @@
 #DESCRIPTION: executes on app_webserver to provision your environment
 
 rm -rf ./shopware
-./sw.phar install:release -r latest -i ./shopware --db-host __DB_HOST__ --db-user __DB_USER__ --db-password __DB_PASSWORD__ --db-name __DB_NAME__ --shop-host __SW_HOST__
+./sw.phar install:release -r __SWVERSION__ -i ./shopware --db-host __DB_HOST__ --db-user __DB_USER__ --db-password __DB_PASSWORD__ --db-name __DB_NAME__ --shop-host __SW_HOST__
